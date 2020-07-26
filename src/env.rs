5d9f197e1f898fcc6b2e606ce6b6267d2fbf815d
Cameron Eldridge <cameldridge@gmail.com>
2020-07-26 11:54:05 -0400

    Delete stuff here too

--- src/env.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(feature="wasm")]
-pub(crate) fn env(name: &str) -> &String {
-    crate::wasm::env(name).unwrap()
-}
-
-#[cfg(feature="native")]
-pub(crate) fn env(name: &str) -> String {
-    std::env::var(name).unwrap()
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,1 @@
-#[macro_use]
-extern crate rental;
 
-mod api;
-pub(crate) mod crypto;
-mod env;
-mod localization;
-
-#[cfg(feature="wasm")] mod wasm;
-#[cfg(feature="wasm")] pub use crypto::*;
-#[cfg(feature="wasm")] pub use wasm::*;
-
-#[cfg(feature="native")] pub use api::*;
-#[cfg(feature="native")] pub use localization::*;
